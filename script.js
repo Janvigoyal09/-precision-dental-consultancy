7abda01454e4fc6c1134d21af0203b5955a8a693
Janvi Goyal <janvigoyal945@gmail.com>
2026-06-01 00:56:50 +1000

    initial website commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.style.boxShadow =
+        "0 10px 30px rgba(0,0,0,0.08)";
+
+    }
+
+    else{
+
+        navbar.style.boxShadow = "none";
+
+    }
+
+});
